32ac810c78629e59e6b5373f724cfca61c0e1891
szaqal <malczyk.pawel@gmail.com>
2023-08-22 18:32:02 +0200

    Work

--- basic_nn/basic_nn.py
+++ basic_nn/basic_nn.py
@@ -9,10 +9,13 @@ def show_learning():
               '%5.2f' % w[0], ', w1=', '%5.2f' % w[1], ', w2=', '%5.2f' % w[2])
 
 
-# XOR function
+#
+# XOR function learning
+#
 np.random.seed(3)
 LEARNING_RATE = 0.1
 index_list = [0, 1, 2, 3]
+
 
 x_train = [
     np.array([1.0, -1.0, -1.0]),
@@ -20,13 +23,13 @@ x_train = [
     np.array([1.0, 1.0, -1.0]),
     np.array([1.0, 1.0, 1.0])
 ]
-
 y_train = [0.0, 1.0, 1.0, 0.0]
 
 
 def neuron_w(input_count):
-    wights = np.zeros(input_count+1)
+    wights = np.zeros(input_count+1)  # array([0., 0.])
     for i in range(1, (input_count+1)):
+        # Random weights to start with
         wights[i] = np.random.uniform(-1.0, 1.0)
     return wights
 
@@ -40,6 +43,8 @@ def forward_pass(x):
     global n_y
     n_y[0] = np.tanh(np.dot(n_w[0], x))
     n_y[1] = np.tanh(np.dot(n_w[1], x))
+
+    # n2 is result neuron single with two inputs
     n2_inputs = np.array([1.0, n_y[0], n_y[1]])  # 1.0 bias
     z2 = np.dot(n_w[2], n2_inputs)
     n_y[2] = 1.0 / (1.0 + np.exp(-z2))
@@ -48,6 +53,7 @@ def forward_pass(x):
 def backward_pass(y_truth):
     global n_error
     error_prime = -(y_truth - n_y[2])
+
     derivative = n_y[2] * (1.0 - n_y[2])
     n_error[2] = error_prime * derivative
 
@@ -62,8 +68,9 @@ def adjust_weigths(x):
     global n_w
     n_w[0] -= (x * LEARNING_RATE * n_error[0])
     n_w[1] -= (x * LEARNING_RATE * n_error[1])
-    n2_inputs = np.array([1.0, n_y[0], n_y[1]]) # 1.0 bias
-    n_w[2] -= (n2_inputs * LEARNING_RATE * n_error[2])
+
+    # n2_inputs = np.array([1.0, n_y[0], n_y[1]])  # 1.0 bias
+    n_w[2] -= (x * LEARNING_RATE * n_error[2])
 
 
 if __name__ == '__main__':
@@ -78,9 +85,9 @@ if __name__ == '__main__':
             show_learning()
         for i in range(len(x_train)):
             forward_pass(x_train[i])
-            print('x1 = ', '%4.1f' % x_train[i][1], ', x2 = ', '%4.1f' % x_train[i][2], ', y = ', '%4.1f' % n_y[2])
-            if(((y_train[i]<0.6) and (n_y[2]>=0.5)) or ((y_train[i] >=0.5) and (n_y[2]< 0.5))):
+
+            print('x1 = ', '%4.1f' % x_train[i][1], ', x2 = ',
+                  '%4.1f' % x_train[i][2], ', y = ', '%.4f' % n_y[2])
+            
+            if(((y_train[i] < 0.6) and (n_y[2] >= 0.5)) or ((y_train[i] >= 0.5) and (n_y[2] < 0.5))):
                 all_correct = True
-
-
-
