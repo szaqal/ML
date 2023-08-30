63ec3e7627e5ff75f4bca9ac36f5ed51b17943c0
szaqal <malczyk.pawel@gmail.com>
2023-08-30 14:30:41 +0200

    Mnist

--- mnist/mnist_tf.py
+++ mnist/mnist_tf.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
 
-
+import os
 import tensorflow as tf
 from tensorflow import keras
 from keras.utils import to_categorical
 from keras import layers
+from plot_training import plot_loss, plot_accuracy
 import numpy as np
 import logging
 
 
 tf.get_logger().setLevel(logging.ERROR)
 tf.random.set_seed(7)
-EPOCHS=20
-BATCH_SIZE=1 # Update weights after each training example
+
+EPOCHS=int(os.getenv("EPOCHS"))
+BATCH_SIZE=int(os.getenv("BATCH_SIZE"))
+LOSS_FUNCTION= os.getenv("LOSS_FUNCTION")
+BIAS = os.getenv("BIAS")
+KERNEL_INITIALIZER=os.getenv("KERNEL_INITIALIZER")
+LEARNING_RATE=float(os.getenv("LEARNING_RATE"))
+OPTIMIZER=os.getenv("OPTIMIZER")
+
+initializer = None
+if KERNEL_INITIALIZER == "random_uniform":
+    initializer = keras.initializers.RandomUniform(minval=0.1, maxval=0.1)
+elif KERNEL_INITIALIZER == "glorot_uniform":
+    initializer = KERNEL_INITIALIZER
+
+
+
+
+print("*"*30)
+print(f"Initializer: {initializer}")
+
+
+
+
 
 
 mnist = keras.datasets.mnist
@@ -28,24 +51,27 @@ train_labels = to_categorical(train_labels, num_classes=10)
 test_labels = to_categorical(test_labels, num_classes=10)
 
 
-initializer = keras.initializers.RandomUniform(minval=0.1, maxval=0.1)
-
-
-
-
 
 strategy = tf.distribute.MirroredStrategy()
-print('Number of devices: {}'.format(strategy.num_replicas_in_sync)) #Works
+print(f'Number of devices: {strategy.num_replicas_in_sync}')
 
 with strategy.scope():
     model = keras.Sequential([        
         layers.Flatten(input_shape=(28,28)), # redudant if data already in singel dimention array
-        layers.Dense(25, activation='tanh', kernel_initializer = initializer, bias_initializer='zeros'),  #Dense fully connected
+        layers.Dense(25, activation='tanh', kernel_initializer = initializer, bias_initializer=BIAS),  #Dense fully connected
         layers.BatchNormalization(),
-        layers.Dense(10, activation='sigmoid', kernel_initializer = initializer, bias_initializer='zeros')
+        layers.Dense(10, activation='sigmoid', kernel_initializer = initializer, bias_initializer=BIAS)
     ])
-    opt = keras.optimizers.SGD(learning_rate=0.01)
-    model.compile(loss='mean_squared_error', optimizer=opt, metrics=['accuracy'])
+
+    opt = None
+    if OPTIMIZER == "sgd":
+        opt = keras.optimizers.SGD(learning_rate=LEARNING_RATE)
+    elif OPTIMIZER =="adam":
+        opt = OPTIMIZER
+
+    print(f"{opt}")
+    print("*"*30)
+    model.compile(loss=LOSS_FUNCTION, optimizer=opt, metrics=['accuracy'])
 
 history = model.fit(
         train_images, 
@@ -56,3 +82,6 @@ history = model.fit(
         verbose=2, 
         shuffle=True
 )
+
+plot_loss(history)
+plot_accuracy(history)
---
+++ mnist/plot_training.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def plot_accuracy(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
