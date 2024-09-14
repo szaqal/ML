dec40c6b9d5714298eb87c0e4d3aae50d19188c2
szaqal <malczyk.pawel@gmail.com>
2024-09-14 13:54:49 +0200

    Misc

--- playground/tensorflow/mnist.py
+++ playground/tensorflow/mnist.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
 
+
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
@@ -12,6 +14,7 @@ model = tf.keras.models.Sequential([
       tf.keras.layers.Dropout(0.2),
       tf.keras.layers.Dense(10)
 ])
+
 
 
 predictions = model(x_train[:1]).numpy()
---
+++ playground/tensorflow/mnist_fashion.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+#matplotlib.use('TkAgg')
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+
+#plt.figure()
+#plt.imshow(train_images[0])
+#plt.colorbar()
+#plt.grid(False)
+#plt.savefig('img.png', dpi=300, bbox_inches='tight')
+#plt.show()
+
+#Scale down
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model,
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+print(predictions)
