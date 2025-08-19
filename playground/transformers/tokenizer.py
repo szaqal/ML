915a0bba57c6837c116741d345e9054a045ab006
szaqal <malczyk.pawel@gmail.com>
2025-08-19 16:58:10 +0200

    Misc

--- playground/transformers/tokenizer.py
+++ playground/transformers/tokenizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from transformers import AutoTokenizer
+from transformers import BertTokenizer
 
 checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
@@ -10,4 +11,11 @@ raw_inputs = [
 inputs = tokenizer(raw_inputs, padding=True, truncation=True, return_tensors = "pt")
 print(inputs)
 
+
+tokenizer2 = BertTokenizer.from_pretrained("bert-base-cased")
+inputs = tokenizer2(raw_inputs, padding=True, truncation=True, return_tensors = "pt")
+print(inputs)
+
+print(tokenizer2.tokenize("Yo yo come check the crazy flow!!"))
+print(tokenizer2.tokenize("Availability is what makes stuff available"))
 
